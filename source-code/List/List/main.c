64677eace2d824d725c4e6dcfc63be140cadc6b0
heooos <hao1993624@gmail.com>
2018-09-17 18:53:55 +0800

    initial proj

---
+++ source-code/List/List/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  List
+//
+//  Created by zh on 2018/9/17.
+//  Copyright © 2018年 zh. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
